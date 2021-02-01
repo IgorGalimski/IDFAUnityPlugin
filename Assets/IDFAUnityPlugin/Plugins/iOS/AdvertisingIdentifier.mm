609b5e3f2c6511ee59c4643a84b47b0797d2a181
IgorGalimski <igorg@playtika.com>
2021-02-01 15:30:48 +0300

    Fix singing build for unity 2019

--- Assets/IDFAUnityPlugin/Plugins/iOS/AdvertisingIdentifier.mm
+++ Assets/IDFAUnityPlugin/Plugins/iOS/AdvertisingIdentifier.mm
@@ -9,24 +9,24 @@
 #import <AdSupport/ASIdentifierManager.h>
 #import <AppTrackingTransparency/AppTrackingTransparency.h>
 
-extern bool IsNeedToRequestIDFAInternal()
+extern "C" bool IsNeedToRequestIDFAInternal()
 {
     return [[TrackingManager sharedInstance] IsNeedToRequestIDFA];
 }
 
-extern char* GetIDFAInternal()
+extern "C" char* GetIDFAInternal()
 {
     return [[TrackingManager sharedInstance] GetIDFA];
 }
 
 API_AVAILABLE(ios(14))
-extern ATTrackingManagerAuthorizationStatus* GetAuthorizationStatusInternal()
+extern "C" ATTrackingManagerAuthorizationStatus* GetAuthorizationStatusInternal()
 {
     return [[TrackingManager sharedInstance] GetAuthorizationStatus];
 }
 
 API_AVAILABLE(ios(14))
-extern void RequestAuthorizationInternal(ATTrackingManagerAuthorizationStatusCallback callback)
+extern "C" void RequestAuthorizationInternal(ATTrackingManagerAuthorizationStatusCallback callback)
 {
     return [[TrackingManager sharedInstance] RequestAuthorization:callback];
 }
